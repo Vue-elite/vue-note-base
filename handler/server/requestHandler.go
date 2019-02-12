f7ac1bc14670ac1753587c4da1ce6aaa11506be0
Akshay Kumar Sharma <kumarsharmaa69@gmail.com>
2019-02-12 21:38:28 +0530

    VN-2 | Added implementation to retrive register request data.

--- handler/server/requestHandler.go
+++ handler/server/requestHandler.go
@@ -1,9 +1,42 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type RegisterUser struct {
+	FirstName   string  `json:"firstName"`
+	LastName string `json:"lastName"`
+	EmailId string `json:"emailId"`
+	Password string `json:"password"`
+	PasswordConfirmation string `json:"passwordConfirmation"`
+}
 
 func handleRegister(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Registeration page</h1>"))
+	if r.Method == "POST" {
+		var u RegisterUser
+		if r.Body == nil {
+			http.Error(w, "Please send a request body", 400)
+			return
+		}
+		fmt.Println(r.Body)
+		err := json.NewDecoder(r.Body).Decode(&u)
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		} else {
+			fmt.Println(u.FirstName)
+			fmt.Println(u.LastName)
+			fmt.Println(u.EmailId)
+			fmt.Println(u.Password)
+			fmt.Println(u.PasswordConfirmation)
+
+			w.Write([]byte("<h1>Registeration page</h1>"))
+		}
+	}
+
 }
 
 func handleSignIn(w http.ResponseWriter, r *http.Request) {
